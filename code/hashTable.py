fe9deeb7f2620425e3f929528ecba0db5250e4ad
MKFMIKU <mikumkf@outlook.com>
2017-02-20 14:25:09 +0800

    RAISR

--- code/hashTable.py
+++ code/hashTable.py
@@ -2,13 +2,15 @@
 
 import numpy as np
 
+from sklearn import preprocessing
+
 def hashTable(patch,Qangle,Qstrenth,Qcoherence):
    
     # Composed G from the horizontal and vertical gradients,
     # gx and gy, of the surroundings of the k-th pixel
     [gx,gy] = np.gradient(patch)
-    gx = gx.ravel()
-    gy = gy.ravel()
+    gx = preprocessing.normalize(gx).ravel()
+    gy = preprocessing.normalize(gy).ravel()
     G = np.matrix((gx,gy))
 
     # Use eigen-decomposition 
@@ -19,15 +21,15 @@ def hashTable(patch,Qangle,Qstrenth,Qcoherence):
     angle = np.math.atan2(eigenvectors[0,1],eigenvectors[0,0])
     if angle<0:
         angle += np.pi
-    angle=np.ceil(angle/Qangle)
+    angle= np.ceil(angle/Qangle)
           
     # Computer the strength
-    stength = np.ceil((eigenvalues.max()/Qstrenth))
+    strength = np.ceil(eigenvalues.max()/Qstrenth)
     
     # Computer the coherence
     lamda1 = np.math.sqrt(eigenvalues.max())
-    lamda2 = np.math.sqrt(eigenvalues[1] if eigenvalues[0] == eigenvalues.max() else eigenvalues[0])
-    coherence = np.abs((lamda1-lamda2)/(lamda1+lamda2))
-    coherence = (coherence/Qcoherence)
+    lamda2 = np.math.sqrt(eigenvalues.min())
+    coherence = 0 if lamda1+lamda2==0 else np.abs((lamda1-lamda2)/(lamda1+lamda2))
+    coherence = np.ceil(coherence/Qcoherence)
     
-    return angle,stength,coherence
+    return angle,strength,coherence
--- code/train.py
+++ code/train.py
@@ -22,74 +22,61 @@ for parent,dirnames,filenames in os.walk(dataDir):
     for filename in filenames:    
         fileList.append(os.path.join(parent,filename)) 
 
-# Code single test        
-mat = cv2.imread(fileList[0],0)
-mat = cv2.normalize(mat.astype('float'), None, 0.0, 1.0, cv2.NORM_MINMAX)
-H,W = mat.shape[:2]
-
-# Use upsampling to get the LRimage
-# which the HRImage is the origin
-LRImage =cv2.resize(mat,(0,0),fx=0.5,fy=0.5)
-HRImage = mat
-
-# Upscale the LRImage
-LRImage = cv2.resize(LRImage,(W,H),cv2.INTER_LINEAR)
-
+# Init the Q and V        
 Q = np.zeros((Qangle*Qstrenth*Qcoherence,R*R,patchSize*patchSize,patchSize*patchSize))
 V = np.zeros((Qangle*Qstrenth*Qcoherence,R*R,patchSize*patchSize))
-# for each pixel k in yi do
-i1 = 0
-while i1+patchSize<H:
-    j1 = 0
-    while j1+patchSize<W:
-        patch = LRImage[i1:i1+patchSize,j1:j1+patchSize]
-        j = [angle,stength,coherence] = hashTable(patch,Qangle,Qstrenth,Qcoherence)
+
+for file in fileList:
+    mat = cv2.imread(file,0)
+    HMat,WMat = mat.shape[:2]
+    
+    # Use upsampling to get the LRimage
+    # which the HRImage is the origin
+    LRImage =cv2.resize(mat,(0,0),fx=0.5,fy=0.5)
+    HRImage = mat
+
+    # Upscale the LRImage
+    LRImage = cv2.resize(LRImage,(WMat,HMat),cv2.INTER_LINEAR)
+
+    # for each pixel k in yi do
+    iPixel = 5
+    while iPixel<HMat-5:
+        jPixel = 5
+        while jPixel<WMat-5:
+            patch = LRImage[iPixel-5:iPixel+5,jPixel-5:jPixel+5]
+            [angle,strength,coherence] = hashTable(patch,Qangle,Qstrenth,Qcoherence)
+            j = angle+strength+coherence
+            patch = patch.reshape(1,-1)
+            x = HRImage[iPixel,jPixel]
+            t = iPixel%R*R+jPixel%R
+            Q[j,t] = Q[j,t]+patch.T*patch
+            V[j,t] = V[j,t]+patch*x
+            jPixel = jPixel+1
+        iPixel = iPixel+1
+
+
+H = np.zeros((Qangle*Qstrenth*Qcoherence,R*R,patchSize*patchSize))    
+# For each key j and pixel-type t        
+for t in range(R*R):
+    for j in range(Qangle*Qstrenth*Qcoherence):
+        reg = linear_model.LinearRegression()
+        reg.fit(Q[j,t],V[j,t])
+        H[j,t] = reg.coef_
+
+print("Train is off\nTest is begin \n")
+
+iPixel = 5
+while iPixel<HMat-5:
+    jPixel = 5
+    while jPixel<WMat-5:
+        patch = LRImage[iPixel-5:iPixel+5,jPixel-5:jPixel+5]
+        [angle,strength,coherence] = hashTable(patch,Qangle,Qstrenth,Qcoherence)
+        j = angle+strength+coherence
+        t = iPixel%R*R+jPixel%R
+        h = H[j,t]
         patch = patch.reshape(1,-1)
-        x = HRImage[i1,j1]
-        t = i1%R*R+j1%R+1
-        A = patch.T*patch
-        b = patch.T*x
-        #Q[j,t] = Q[j,t]+A
-        #V[j,t] = V[j,t]+b
-        #for each
-        j1 = j1+patchSize
-    i1 = i1+patchSize
-
-# for each key j and pixel-type t
-#for r in R*R:
-#    for j in Qangle*Qstrenth*Qcoherence:
-        
-                
-#np.concatenate((Q,A),axis=0)
-
-# Use eigen-decomposition to computer angel
-
-# Use scikit-learn library to get least-squares
-# dataset_Q = []
-# dataset_V = []
-
-'''
-A = np.empty(((H-10)*(W-10),100))
-for i1 in range(H-10):
-    for j1 in range(W-10):
-        patchL = LRImage[i1:i1+10,j1:j1+10]
-        patchL = patchL.ravel().T                     
-        A[i1*j1]=patchL
-Q = A.T*A
-V = A.T*HRImage    
-'''
-     
-# A = np.matrix(A)        
-# Sampling K patches/pixels from the images
-
-'''
-b = HRImage[0:10,0:10]
-Q = A.T*A
-'''
-
-# dataset_Q.append(Q)
-# dataset_V.append(V)    
-        
-# dataset_Q = np.array(dataset_Q)
-# regr = linear_model.LinearRegression()
-# regr.fit(Q, V)
+        LRImage[iPixel,jPixel] = patch*np.matrix(h).T   
+        jPixel = jPixel+1
+    iPixel = iPixel+1
+    
+cv2.imwrite("Result.bmp",LRImage)
